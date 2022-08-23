b8693a3920cc56160894663493b32425fe8fd15c
yoonki <soundware@gmail.com>
2022-08-23 19:28:30 +0900

    Update test.py

--- test.py
+++ test.py
@@ -4,6 +4,6 @@ import streamlit as st
 st.markdown('쿠팡은 (https://www.togle.io/) 이용하세요.')
 form = st.form(key="submit-form")
 input_text = re.sub(r"수취인명|연락처.|배송지|정보|배송메모|배송구분|국내배송|수령자명|수령자정보수정|..전화|주소|수정|--", ",", form.text_input("네이버, 카페24 배송주소를 복사에서 입력 하세요"))
-input_text = input_text.replace('\t\t','').replace('\t','\n').replace('\n,\n','\n').replace(',','').replace('\n \n','\n')
+input_text = input_text.replace('\t\t','').replace('\t','\n').replace('\n,\n','\n').replace(',','').replace('\n \n','\n').replace('--\n','')
 generate = form.form_submit_button("Generate")
 st.text(input_text)
