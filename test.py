c4631e13c3a009a863a11a3355f72b066bab6b38
yoonki <soundware@gmail.com>
2024-01-12 11:22:37 +0900

    Update test.py

--- test.py
+++ test.py
@@ -2,48 +2,47 @@ import streamlit as st
 import re
 
 def extract_product_info(text):
-    pattern = r'상품명(.*?)상품주문상태'
-    match = re.search(pattern, text, re.DOTALL)
-    return match.group(1).strip() if match else "상품명 및 주문 상태를 찾을 수 없습니다."
+    product_pattern = re.compile(r'상품명(.*?)상품주문상태', re.DOTALL)
+    product_match = product_pattern.search(text)
+    return product_match.group(1).strip() if product_match else "상품명 및 주문 상태를 찾을 수 없습니다."
 
 def extract_recipient_info(text):
-    pattern = r'수취인명(.*?)연락처1(.*?)연락처2'
-    match = re.search(pattern, text, re.DOTALL)
-    if match:
-        recipient_info = match.group(1).strip()
-        contact1 = match.group(2).strip()
-        return recipient_info, contact1
+    recipient_info_pattern = re.compile(r'수취인명(.*?)연락처1(.*?)연락처2', re.DOTALL)
+    recipient_info_match = recipient_info_pattern.search(text)
+    if recipient_info_match:
+        recipient_info_result = recipient_info_match.group(1).strip()
+        contact1_result = recipient_info_match.group(2).strip()
+        return recipient_info_result, contact1_result
     else:
         return "수취인명, 연락처1, 연락처2를 찾을 수 없습니다.", ""
 
-def extract_delivery_info(text, recipient_info, contact1):
-    info_delivery_text = re.sub(f'{recipient_info}.*?{contact1}', '', text, flags=re.DOTALL)
-    pattern = r'배송지(.*?)배송메모'
-    match = re.search(pattern, info_delivery_text, re.DOTALL)
-    if match:
-        delivery_info = match.group(1).strip().replace('정보 배송지 정보 수취인명\t\t연락처2\t 배송지', '').strip()
-        return delivery_info
+def extract_delivery_info(text, recipient_info_result, contact1_result):
+    info_delivery_text = re.sub(f'{recipient_info_result}.*?{contact1_result}', '', text, flags=re.DOTALL)
+    delivery_info_pattern = re.compile(r'배송지(.*?)배송메모', re.DOTALL)
+    delivery_info_match = delivery_info_pattern.search(info_delivery_text)
+    if delivery_info_match:
+        delivery_info_result = delivery_info_match.group(1).strip()
+        return delivery_info_result.replace('정보 배송지 정보 수취인명\t\t연락처2\t 배송지','').strip()
     else:
         return "배송지 및 배송메모를 찾을 수 없습니다."
 
 def main():
-    st.title("스.스 주소")
-    text = st.text_area("Enter text:", height=200)
-
+    st.title("스마트스토어 주문 주소 복사")
+    text = st.text_area("Enter text:")
+    
     if st.button("Extract Information"):
         product_result = extract_product_info(text)
-        recipient_info, contact1 = extract_recipient_info(text)
-        delivery_info = extract_delivery_info(text, recipient_info, contact1)
+        recipient_info_result, contact1_result = extract_recipient_info(text)
+        delivery_info_result = extract_delivery_info(text, recipient_info_result, contact1_result)
 
         st.text(product_result)
-        st.text(contact1)  # Displaying only the contact information
-        st.text(delivery_info)
 
-        # Add a line break for easy pasting and searching
-        st.text("")
 
-        # Display the message about checking options and quantity
-        st.subtitle("옵션, 수량 확인 .")
+        st.text(recipient_info_result)
+        st.text(contact1_result)
+
+
+        st.text(delivery_info_result)
 
 if __name__ == "__main__":
     main()
